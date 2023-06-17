9e198a0fe2553d13ed721fa5313a9e4aadfb30c3
sefeen <faragsefen2@gmail.com>
2023-06-17 18:06:47 +0300

    desable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp( home: TableScreen());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: TableScreen());
   }
 }
 
